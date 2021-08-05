14dc8a321715760a75b0b49dac48c4b7d0a11bae
Sushant <sushantkumararkey@gmail.com>
2021-08-05 11:22:19 +0530

    modified user.java

--- User.java
+++ User.java
@@ -1,6 +1,6 @@
-class Mycl{
+class User{
       public static void main(String arg[]){
-	  System.out.println("Hello World");
+	  System.out.println("Hello User");
           System.out.println("Hello Again");
 
 
