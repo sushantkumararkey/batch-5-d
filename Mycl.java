7acbe45a17277a74a40762466c06acb18d16567a
Sushant <sushantkumararkey@gmail.com>
2021-08-05 11:45:24 +0530

    modification 2

--- Mycl.java
+++ Mycl.java
@@ -2,7 +2,14 @@ class Mycl{
       public static void main(String arg[]){
 	  System.out.println("Hello World");
           System.out.println("Hello Again");
-
+	
+	  System.out,println(add(5+6));
 
 	  }
+
+	public static int add(int  a , int b){
+        
+	return a+b;
+}
+
 }
